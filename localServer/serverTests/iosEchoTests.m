ed3152217fdb264899e8ba937cf6125fc45fcdf6
Andrew Yang <andrewyang@Andrew-Yangs-MacBook-Pro.local>
2014-11-12 17:01:01 -0600

    more reorganizing

---
+++ localServer/serverTests/iosEchoTests.m
@@ -0,0 +1,34 @@
+//
+//  iosEchoTests.m
+//  iosEchoTests
+//
+//  Created by Zachary Jenkins on 11/9/14.
+//  Copyright (c) 2014 Zachary Jenkins. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iosEchoTests : XCTestCase
+
+@end
+
+@implementation iosEchoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
